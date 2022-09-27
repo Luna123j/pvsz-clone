daa791791e617987463a5817bd7dc581fbbb9b1e
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-09-27 12:34:14 -0400

    try canvas for html

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Canvas from './components/Canvas';
 
 function App() {
+  const draw = context => {
+    context.beginPath();
+    context.arc(95, 50, 40, 0, 2 * Math.PI);
+    context.stroke();
+  };
   return (
     <div className="App">
+      <Canvas draw={draw} height={100} width={100}></Canvas>
     </div>
   );
 }
